0e5b27e9a4d33d42573966bdf47509095ea1cef6
DoctorFinger <144325868+DoctorFinger@users.noreply.github.com>
2026-08-25 02:31:12 +0300

    diagnosis

--- patientor/backend/src/index.ts
+++ patientor/backend/src/index.ts
@@ -1,5 +1,8 @@
 import express from "express";
+import patientsRouter from "./routes/patients.ts";
+
 const app = express();
+
 app.use(express.json());
 
 const PORT = 3001;
@@ -9,6 +12,7 @@ app.get("/api/ping", (_req, res) => {
   res.send("pong");
 });
 
+app.use("/api/patients", patientsRouter);
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
   console.log(`Server address: http://localhost:${PORT}/api/ping`);
