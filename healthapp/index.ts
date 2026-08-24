6a8546a01936851681416cf4dd2c82526c7d2d83
DoctorFinger <144325868+DoctorFinger@users.noreply.github.com>
2026-08-24 11:14:30 +0300

    get exercises api

--- healthapp/index.ts
+++ healthapp/index.ts
@@ -1,6 +1,6 @@
-import express from "express";
+import express, { Request, Response } from "express";
 import calculateBmi from "./bmiCalculator.ts";
-
+import calculateExercises from "./exerciseCalculator.ts";
 const app = express();
 const PORT = 3003;
 
@@ -18,6 +18,28 @@ app.get("/bmi", (req, res) => {
 
   const bmi = calculateBmi(height, weight);
   return res.status(200).json({ height, weight, bmi });
+});
+
+app.get("/exercises", (req: Request, res: Response) => {
+  // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
+  const { daily_exercixes, target } = req.body;
+
+  if (!daily_exercixes || !target) {
+    return res.status(400).json({ error: "parameters missing" });
+  }
+  if (!Array.isArray(daily_exercixes)) {
+    return res.status(400).json({ error: "malformatted parameters" });
+  }
+  if (typeof target !== "number") {
+    return res.status(400).json({ error: "malformatted parameters" });
+  }
+  for (const number of daily_exercixes) {
+    if (typeof number !== "number") {
+      return res.status(400).json({ error: "malformatted parameters" });
+    }
+  }
+  const result = calculateExercises(daily_exercixes as number[], target);
+  return res.status(200).json({ ...result });
 });
 
 app.listen(PORT, () => {
